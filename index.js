serverless-community-labs/serverless-plugin-simulate

commit 08929ff0fbc765f2ae1b3fabdc370f04f0befff3

mocking supporting services.

index.js
@@ -7,6 +7,7 @@ const run = require('./lib/run')
 const config = require('./lib/config')
 const lambda = require('./lib/lambda')
 const serve = require('./lib/serve')
+const services = require('./lib/services')
 
 const apiGatewayConfig = {
   usage: 'Simulate the API Gateway and serves λ locally',
@@ -49,18 +50,34 @@ class Simulate {
           },
         },
         commands: {
+          services: {
+            usage: 'Start supporting services',
+            lifecycleEvents: [
+              'start',
+            ],
+            options: {
+              file: {
+                usage: 'Specify an alternate compose file. Default: docker-compose.yml',
+                shortcut: 'f',
+              },
+              host: {
+                usage: 'Docker daemon socket to connect to.',
+                shortcut: 'h',
+              },
+            },
+          },
           invoke: {
             usage: 'Run a λ function locally',
             lifecycleEvents: [
               'invoke',
             ],
             options: {
-              function: {
+              'dc-file': {
                 usage: 'Name of the function',
                 shortcut: 'f',
                 required: true,
               },
-              path: {
+              'dc-host': {
                 usage: 'Path to JSON file holding input data',
                 shortcut: 'p',
               },
@@ -103,27 +120,51 @@ class Simulate {
 
     this.hooks = {
       'simulate:invoke:invoke': () => BbPromise.bind(this)
+        .then(this.servicesStart)
         .then(this.run)
         .then(out => this.serverless.cli.consoleLog(out)),
+
+      'simulate:services:start': () => BbPromise.bind(this)
+        .then(this.servicesStart),
 
       'simulate:register:register': () => BbPromise.bind(this)
         .then(this.register),
 
       'simulate:lambda:start': () => BbPromise.bind(this)
+        .then(this.servicesStart)
         .then(this.lambda),
 
       'simulate:serve:initialize': () => BbPromise.bind(this)
         .then(this.apigatewayInit),
 
       'simulate:serve:start': () => BbPromise.bind(this)
+        .then(this.servicesStart)
         .then(this.apigatewayStart),
 
       'simulate:apigateway:initialize': () => BbPromise.bind(this)
         .then(this.apigatewayInit),
 
       'simulate:apigateway:start': () => BbPromise.bind(this)
+        .then(this.servicesStart)
         .then(this.apigatewayStart),
     }
+  }
+
+  servicesStart() {
+    const start = this.serverless.service.custom &&
+        this.serverless.service.custom.simulate &&
+        this.serverless.service.custom.simulate.services
+
+    if (!start) {
+      return
+    }
+
+    const options = {
+      file: this.options['dc-file'],
+      host: this.options['dc-host'],
+    }
+    const logger = this.createLogger()
+    services.start(options, logger)
   }
 
   apigatewayInit() {

lib/services/index.js
@@ -1,0 +1,39 @@
+const spawnSync = require('child_process').spawnSync
+
+const up = options => {
+  const opts = options || {}
+  const file = opts.file || `${process.cwd()}/docker-compose.yml`
+  const host = opts.host
+  const spawnOptions = opts.spawnOptions || { encoding: 'utf8' }
+
+  const args = []
+    .concat(file ? ['--file', file] : [])
+    .concat(host ? ['--host', host] : [])
+    .concat(['up', '-d'])
+
+  const spawnResult = spawnSync('docker-compose', args, spawnOptions)
+
+  if (spawnResult.error || spawnResult.status !== 0) {
+    let err = spawnResult.error
+    if (!err) {
+      err = new Error(spawnResult.stdout || spawnResult.stderr)
+      err.code = spawnResult.status
+      err.stdout = spawnResult.stdout
+      err.stderr = spawnResult.stderr
+    }
+    throw err
+  }
+
+  return spawnResult.stderr.trim()
+}
+
+const start = (options, log) => {
+  log('Starting mock services.')
+  log(up(options, log))
+  log('Started mock services.')
+}
+
+// Will spawn `docker-compose up -d` synchronously
+module.exports = {
+  start,
+}

examples/docker-compose.yml
@@ -1,0 +1,8 @@
+version: '2'
+
+services:
+  s3:
+    build: images/moto
+    command: [s3, "-H", "0.0.0.0"]
+    ports:
+      - "5000:5000"

examples/images/moto/Dockerfile
@@ -1,0 +1,12 @@
+FROM python:2.7
+RUN pip install moto[server]
+
+# Create boto endpoints.json with "moto" region
+COPY endpoints.json /opt/moto/
+VOLUME ["/opt/moto"]
+
+# Default port that moto listens on.
+EXPOSE 5000
+
+ENTRYPOINT ["moto_server"]
+CMD ["s3"]

examples/serverless.yml
@@ -8,6 +8,10 @@ provider:
 plugins:
   - serverless-plugin-simulate
 
+custom:
+  simulate:
+    services: true
+
 functions:
   authorizer:
     handler: functions/authorizer.handler

commit 42d644b8dd06f8e5711cf2513293b8fb9b321137

fix serverless simulate invoke
The parameter names were broken in 08929ff0, this sets them back to the right
values.

index.js
@@ -76,12 +76,12 @@ class Simulate {
               'invoke',
             ],
             options: {
-              'dc-file': {
+              function: {
                 usage: 'Name of the function',
                 shortcut: 'f',
                 required: true,
               },
-              'dc-host': {
+              path: {
                 usage: 'Path to JSON file holding input data',
                 shortcut: 'p',
               },